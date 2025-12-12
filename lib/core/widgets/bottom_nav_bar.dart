1be96cbf97ea11af3b47df07f918aa43ee45bd85
Marwa Eltayeb <marwa_eltayeb@yahoo.com>
2025-12-12 16:10:00 +0200

    Improve bottom nav navigation

--- lib/core/widgets/bottom_nav_bar.dart
+++ lib/core/widgets/bottom_nav_bar.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import '../../config/routing/routes.dart';
 import '../../config/theme/app_colors.dart';
+import '../constants/app_strings.dart';
 import 'nav_bar_item.dart';
 
 class BottomNavBar extends StatelessWidget {
-
   final int currentIndex;
 
   const BottomNavBar({
@@ -12,16 +12,41 @@ class BottomNavBar extends StatelessWidget {
     required this.currentIndex,
   });
 
+  void _navigateToScreen(BuildContext context, int index) {
+
+    if (currentIndex == index) return;
+
+    String routeName;
+    switch (index) {
+      case 0:
+        routeName = Routes.home;
+        break;
+      case 1:
+        routeName = Routes.market;
+        break;
+      case 2:
+        routeName = Routes.portfolio;
+        break;
+      case 3:
+        routeName = Routes.settings;
+        break;
+      default:
+        return;
+    }
+
+    Navigator.pushNamedAndRemoveUntil(context, routeName, (route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
         height: 70,
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: AppColors.white,
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.05),
+              color: AppColors.black.withOpacity(0.05),
               blurRadius: 10,
               offset: const Offset(0, -2),
             ),
@@ -32,35 +57,27 @@ class BottomNavBar extends StatelessWidget {
           children: [
             NavBarItem(
               icon: Icons.home,
-              label: 'Home',
+              label: AppStrings.homeScreenTitle,
               isSelected: currentIndex == 0,
-              onTap: () {
-                Navigator.pushReplacementNamed(context, Routes.home);
-              },
+              onTap: () => _navigateToScreen(context, 0),
             ),
             NavBarItem(
               icon: Icons.bar_chart,
-              label: 'Market',
+              label: AppStrings.marketScreenTitle,
               isSelected: currentIndex == 1,
-              onTap: () {
-                Navigator.pushReplacementNamed(context, Routes.market);
-              },
+              onTap: () => _navigateToScreen(context, 1),
             ),
             NavBarItem(
               icon: Icons.business_center,
-              label: 'Portfolio',
+              label: AppStrings.portfolioScreenTitle,
               isSelected: currentIndex == 2,
-              onTap: () {
-                Navigator.pushReplacementNamed(context, Routes.portfolio);
-              },
+              onTap: () => _navigateToScreen(context, 2),
             ),
             NavBarItem(
               icon: Icons.settings,
-              label: 'Settings',
+              label: AppStrings.settingsScreenTitle,
               isSelected: currentIndex == 3,
-              onTap: () {
-                Navigator.pushReplacementNamed(context, Routes.settings);
-              },
+              onTap: () => _navigateToScreen(context, 3),
             ),
           ],
         ),
@@ -68,4 +85,3 @@ class BottomNavBar extends StatelessWidget {
     );
   }
 }
-
